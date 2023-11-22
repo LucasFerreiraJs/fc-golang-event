d621ab16475f889ad4d243727fb39e86856cca35
Lucas Ferreira <casferreiraa@gmail.com>
2023-11-21 21:34:05 -0300

    (rabbitmq): add consumer and producer

---
+++ pkg/cmd/producer/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fcutils/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	rabbitmq.Publish(ch, "Hello world!", "amq.direct")
+}
